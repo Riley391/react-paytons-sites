2a56a489e5f345d4976ba1a300305a143e354e11
Payton <paytonl@duck.com>
2025-02-22 21:32:05 -0700

    generating site cards programmatically

--- src/App.js
+++ src/App.js
@@ -6,24 +6,16 @@ const App = () => {
   return (
     <div>
       <h1>Payton's Sites</h1>
-      <SiteCard
-        title={siteList[0].title}
-        link={siteList[0].link}
-        thumbnail={siteList[0].thumbnail}
-        year={siteList[0].year}
-      />
-      <SiteCard
-        title={siteList[1].title}
-        link={siteList[1].link}
-        thumbnail={siteList[1].thumbnail}
-        year={siteList[1].year}
-      />
-      <SiteCard
-        title={siteList[2].title}
-        link={siteList[2].link}
-        thumbnail={siteList[2].thumbnail}
-        year={siteList[2].year}
-      />
+      <div>
+        {siteList.map((site) => (
+          <SiteCard
+            title={site.title}
+            link={site.link}
+            thumbnail={site.thumbnail}
+            year={site.year}
+          />
+        ))}
+      </div>
     </div>
   );
 };
