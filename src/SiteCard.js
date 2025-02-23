ef0946fbc7b8763334fd6ea27788885ea2363dab
Payton <paytonl@duck.com>
2025-02-22 20:31:44 -0700

    added site card component

---
+++ src/SiteCard.js
@@ -0,0 +1,22 @@
+const SiteCard = (props) => {
+  return (
+    <a href={props.link}>
+      <div className="site-box">
+        <img
+          src={props.thumbnail}
+          alt={props.title}
+          width="250px"
+          height="125"
+        />
+        <p class="site-title card-text">
+          <strong>{props.title}</strong>
+        </p>
+        <p class="site-year card-text">
+          <em>Est. {props.year}</em>
+        </p>
+      </div>
+    </a>
+  );
+};
+
+export default SiteCard;
